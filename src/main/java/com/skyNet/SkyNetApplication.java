5db3d08f281ef17e426e6f9ef58c47e7780d1dcc
viMoraes10 <viniciusamoare243@gmail.com>
2025-04-08 19:18:42 -0300

    Primeiro commit

---
+++ src/main/java/com/skyNet/SkyNetApplication.java
@@ -0,0 +1,13 @@
+package com.skyNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyNetApplication.class, args);
+	}
+
+}
