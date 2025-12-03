0745721baad544b01ca90d9b767adc1452f4f98a
viMoraes10 <viniciusamoare243@gmail.com>
2025-12-03 17:54:12 -0300

    Implementado os seguinte repository buscarOcorrenciasPorPeriodo buscarOcorrenciasPorMotivo buscarOcorrenciasPorMotivoParcial

--- src/main/java/com/skyNet/repository/OccurrenceRepository.java
+++ src/main/java/com/skyNet/repository/OccurrenceRepository.java
@@ -2,6 +2,22 @@ package com.skyNet.repository;
 
 import com.skyNet.model.Occurrence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface OccurrenceRepository extends JpaRepository<Occurrence, Long> {
+
+    @Query("SELECT o FROM Occurrence o WHERE o.date BETWEEN :dataInicial AND :dataFinal")
+    List<Occurrence> buscarOcorrenciasPorPeriodo(@Param("dataInicial") LocalDate dataInicial,
+                                                 @Param("dataFinal") LocalDate dataFinal);
+
+    @Query("SELECT o FROM Occurrence o WHERE LOWER(o.description) = LOWER(:motivo)")
+    List<Occurrence> buscarOcorrenciasPorMotivo(@Param("motivo") String motivo);
+
+    @Query("SELECT o FROM Occurrence o WHERE LOWER(o.description) LIKE LOWER(CONCAT('%', :trechoMotivo, '%'))")
+    List<Occurrence> buscarOcorrenciasPorMotivoParcial(@Param("trechoMotivo") String trechoMotivo);
+
 }
